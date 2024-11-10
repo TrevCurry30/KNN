7670f2ac45022f35da80bbd35a83989893bcf2bb
TrevCurry30 <29tthomas@ransomeverglades.org>
2024-11-09 20:01:15 -0500

    Update OneNearestNeighbor_Classifier.py

--- OneNearestNeighbor_Classifier.py
+++ OneNearestNeighbor_Classifier.py
@@ -26,8 +26,20 @@ X_train = np.array([[1,3], [4,2], [6,7], [11, 14], [9, 15], [10, 13], [18,20]])
 #X_train = [1, 2, 4, 16, 17, 18, 19]
 y_train = [0, 0, 0, 1, 1, 1, 1]
 X_test = np.array([[-2, 1], [2,4], [6,5], [-10, 23], [10, 12], [15, 12], [18, 21]])
+y_test = [0, 0, 0, 1, 1, 1, 1]
 #X_test = [-2, 6, 10.9, 12, 18.5, 24, 38]
 
 # CLassifying new datapoints
+y_pred = []
 for new in X_test:
-    print(KNN_Classifier(X_train, y_train, new))
+    y_pred.append(KNN_Classifier(X_train, y_train, new))
+print(y_pred)
+
+# Computing accuracy
+n_correct = 0
+n_total = len(y_test)
+for i in range(len(y_pred)):
+    if y_pred[i] == y_test[i]:
+        n_correct += 1
+accuracy = n_correct/n_total
+print(str(accuracy*100) + '% accuracy')
